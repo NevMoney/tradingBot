5ca77d713d9645bfc253ad93cd8bc5abe5cc62b2
Nev <67838965+NevMoney@users.noreply.github.com>
2021-09-20 07:26:31 -0400

    Update index.js
    created simple functions to put orders in , read the account balance and to cancel the orders

--- index.js
+++ index.js
@@ -5,17 +5,66 @@ const secret = process.env.GEMINI_API_SECRET
 const key = process.env.GEMINI_API_KEY
 const restClient = new GeminiAPI({ key, secret, sandbox: true })
 
-// create order
+// check balances
+// restClient
+//   .getMyAvailableBalances()
+//   .then((response) => console.log('balances', response))
+//   .catch((error) => console.log('balance err', error))
+
+// put orders in
+// restClient
+//   .newOrder({
+//     amount: 5,
+//     price: 250,
+//     side: 'buy',
+//     symbol: 'ethusd',
+//   })
+//   .then((resolve) => {
+//     console.log('eth buy', resolve)
+//     orderIds = []
+//     orderIds.push(resolve.order_id)
+//     console.log(orderIds)
+//   })
+//   .catch((error) => console.log('eth buy err', error))
+
+// restClient
+//   .newOrder({
+//     amount: 1,
+//     price: 75500,
+//     side: 'sell',
+//     symbol: 'btcusd',
+//   })
+//   .then((resolve) => {
+//     console.log('btc sell', resolve)
+//     orderIds = []
+//     orderIds.push(resolve.order_id)
+//     console.log(orderIds)
+//   })
+//   .catch((error) => console.log('btc sell err', error))
+
+// cancel orders
+// function cancelMyOrders(orderIds) {
+//   // access orderIds array and the itterate through it and cancel each order
+//   orderIds.forEach((orderId) => {
+//     restClient
+//       .cancelOrder(orderId)
+//       .then((resolve) => console.log('cancel', resolve))
+//       .catch((error) => console.log('cancel err', error))
+//   })
+// }
+
+// cancelMyOrders(orderIds)
+
 restClient
-  .newOrder({
-    amount: 1,
-    price: 15750,
-    side: 'buy',
-    symbol: 'btcusd',
-  })
+  .cancelAllActiveOrders()
   .then((resolve) => {
-    console.log(resolve)
+    console.log('cancel all', resolve)
   })
-  .catch((error) => {
-    console.log(error)
+  .catch((error) => console.log('cancel all err', error))
+
+restClient
+  .getMyActiveOrders()
+  .then((orders) => {
+    console.log('my activer orders', orders)
   })
+  .catch((error) => console.log('active order err', error))
