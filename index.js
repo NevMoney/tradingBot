050b574269616511f1887503cf364e3dd1f6294d
Nev <67838965+NevMoney@users.noreply.github.com>
2021-09-20 07:43:11 -0400

    clean up

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const secret = process.env.GEMINI_API_SECRET
 const key = process.env.GEMINI_API_KEY
 const restClient = new GeminiAPI({ key, secret, sandbox: true })
+const cryptoCompare = process.env.CRYPTOCOMPARE_API_KEY
 
 // check balances
 // restClient
@@ -12,48 +13,15 @@ const restClient = new GeminiAPI({ key, secret, sandbox: true })
 //   .catch((error) => console.log('balance err', error))
 
 // put orders in
-// restClient
-//   .newOrder({
-//     amount: 5,
-//     price: 250,
-//     side: 'buy',
-//     symbol: 'ethusd',
-//   })
-//   .then((resolve) => {
-//     console.log('eth buy', resolve)
-//     orderIds = []
-//     orderIds.push(resolve.order_id)
-//     console.log(orderIds)
-//   })
-//   .catch((error) => console.log('eth buy err', error))
-
-// restClient
-//   .newOrder({
-//     amount: 1,
-//     price: 75500,
-//     side: 'sell',
-//     symbol: 'btcusd',
-//   })
-//   .then((resolve) => {
-//     console.log('btc sell', resolve)
-//     orderIds = []
-//     orderIds.push(resolve.order_id)
-//     console.log(orderIds)
-//   })
-//   .catch((error) => console.log('btc sell err', error))
-
-// cancel orders
-// function cancelMyOrders(orderIds) {
-//   // access orderIds array and the itterate through it and cancel each order
-//   orderIds.forEach((orderId) => {
-//     restClient
-//       .cancelOrder(orderId)
-//       .then((resolve) => console.log('cancel', resolve))
-//       .catch((error) => console.log('cancel err', error))
-//   })
-// }
-
-// cancelMyOrders(orderIds)
+restClient
+  .newOrder({
+    amount: 5,
+    price: 250,
+    side: 'buy',
+    symbol: 'ethusd',
+  })
+  .then((resolve) => console.log('eth buy', resolve))
+  .catch((error) => console.log('eth buy err', error))
 
 restClient
   .cancelAllActiveOrders()
