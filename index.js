1a0a1a11a70375ae5f3f8de140de00c1e8794101
Nev <67838965+NevMoney@users.noreply.github.com>
2021-09-21 09:47:41 -0400

    module for indicators created
    pay attention to the work around for the code to make it work

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-// global.fetch = require('node-fetch')
-import fetch from 'node-fetch'
-
-if (!globalThis.fetch) {
-  globalThis.fetch = fetch
-}
-
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 
 const GeminiAPI = require('gemini-api').default
 require('dotenv').config()
-const CC = require('cryptocompare')
-CC.setApiKey(process.env.CRYPTO_COMPARE_API_KEY)
+
+import indicators from './indicators.js'
+
+// const indicators = require('./indicators.js')
 
 const secret = process.env.GEMINI_API_SECRET
 const key = process.env.GEMINI_API_KEY
 const gemini = new GeminiAPI({ key, secret, sandbox: true })
-
-const indicators = require('./indicators.js')
 
 indicators.movingAverage('ETH', 'USD', 50, function (result) {
   console.log('MA: ', result)
--- indicators.js
+++ indicators.js
@@ -1,21 +1,34 @@
-module.exports = {
-  movingAverage: function (crypto, fiat, hours, callback) {
-    if (hours > 169) {
-      console.error('Only up to 169 hours allowed')
-      return
-    }
-    CC.histoHour(crypto, fiat)
-      .then((data) => {
-        // need to reverse the data
-        const reversedData = data.reverse()
-        var sum = 0
-        for (var i = 0; i < hours; i++) {
-          sum += reversedData[i].close
-        }
-        // #2 calculate MA for past 100 hours
-        const avg = sum / hours
-        callback(avg)
-      })
-      .catch(console.error)
-  },
+import fetch from 'node-fetch'
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch
+}
+
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+
+const CC = require('cryptocompare')
+CC.setApiKey(process.env.CRYPTO_COMPARE_API_KEY)
+
+const movingAverage = (crypto, fiat, hours, callback) => {
+  if (hours > 169) {
+    console.error('Only up to 169 hours allowed')
+    return
+  }
+  CC.histoHour(crypto, fiat) //histoDay(BTC,USD,limit('none'))
+    .then((data) => {
+      // need to reverse the data
+      const reversedData = data.reverse()
+      var sum = 0
+      for (var i = 0; i < hours; i++) {
+        sum += reversedData[i].close
+      }
+      // #2 calculate MA for past 100 hours
+      const avg = sum / hours
+      callback(avg)
+    })
+    .catch(console.error)
+}
+
+export default {
+  movingAverage,
 }
