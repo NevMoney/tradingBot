0d52983fa1ff4d7233a989521ccb458417463698
Nev <67838965+NevMoney@users.noreply.github.com>
2021-09-21 10:28:43 -0400

    more indicators/functions
    added daily and minute moving average indicators
    
    created a market buy function

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
-
 const GeminiAPI = require('gemini-api').default
 require('dotenv').config()
-
 import indicators from './indicators.js'
-
-// const indicators = require('./indicators.js')
 
 const secret = process.env.GEMINI_API_SECRET
 const key = process.env.GEMINI_API_KEY
 const gemini = new GeminiAPI({ key, secret, sandbox: true })
 
-indicators.movingAverage('ETH', 'USD', 50, function (result) {
-  console.log('MA: ', result)
+indicators.hourlyMovingAverage('ETH', 'USD', 30, function (result) {
+  console.log('Hourly MA: ', result)
 })
+
+indicators.dailyMovingAverage('ETH', 'USD', 30, function (result) {
+  console.log('Daily MA: ', result)
+})
+
+indicators.minuteMovingAverage('ETH', 'USD', 30, function (result) {
+  console.log('Minute MA: ', result)
+})
+
+function marketBuy(symbol) {
+  return gemini.newOrder({
+    symbol: symbol,
+    amount: 1,
+    price: 65000,
+    side: 'buy',
+    type: 'exchange limit',
+    options: ['immediate-or-cancel'],
+  })
+}
+
+marketBuy('btcusd')
+  .then((res) => console.log(res))
+  .catch(console.error)
--- indicators.js
+++ indicators.js
@@ -9,7 +9,7 @@ const require = createRequire(import.meta.url)
 const CC = require('cryptocompare')
 CC.setApiKey(process.env.CRYPTO_COMPARE_API_KEY)
 
-const movingAverage = (crypto, fiat, hours, callback) => {
+const hourlyMovingAverage = (crypto, fiat, hours, callback) => {
   if (hours > 169) {
     console.error('Only up to 169 hours allowed')
     return
@@ -29,6 +29,48 @@ const movingAverage = (crypto, fiat, hours, callback) => {
     .catch(console.error)
 }
 
+const dailyMovingAverage = (crypto, fiat, days, callback) => {
+  if (days > 169) {
+    console.error('Only up to 169 days allowed')
+    return
+  }
+  CC.histoDay(crypto, fiat) //histoDay(BTC,USD,limit('none'))
+    .then((data) => {
+      // need to reverse the data
+      const reversedData = data.reverse()
+      var sum = 0
+      for (var i = 0; i < days; i++) {
+        sum += reversedData[i].close
+      }
+      // #2 calculate MA for past 100 days
+      const avg = sum / days
+      callback(avg)
+    })
+    .catch(console.error)
+}
+
+const minuteMovingAverage = (crypto, fiat, minutes, callback) => {
+  if (minutes > 169) {
+    console.error('Only up to 169 minutes allowed')
+    return
+  }
+  CC.histoDay(crypto, fiat) //histoDay(BTC,USD,limit('none'))
+    .then((data) => {
+      // need to reverse the data
+      const reversedData = data.reverse()
+      var sum = 0
+      for (var i = 0; i < minutes; i++) {
+        sum += reversedData[i].close
+      }
+      // #2 calculate MA for past 100 minutes
+      const avg = sum / minutes
+      callback(avg)
+    })
+    .catch(console.error)
+}
+
 export default {
-  movingAverage,
+  hourlyMovingAverage,
+  dailyMovingAverage,
+  minuteMovingAverage,
 }
